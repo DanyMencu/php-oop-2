206fb20c4bcffe3832fba87a7530161d9aefe57c
DanyMencu <dany69men@gmail.com>
2022-01-10 14:47:06 +0100

    Create Premium and Employee class extend from User

--- classes/Users_class/Users.php
+++ classes/Users_class/Users.php
@@ -16,4 +16,76 @@ class User {
         $this->last_name = $last_name;
         $this->age = $age;
     }
+
+    //Full name function
+    public function getFullName() { 
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
+    //Set sale if it's over 65
+    protected function setSale() {
+        $this->sale = $this->age > 65 ? 10 : 0;
+    }
+
+    public function getSale() {
+        $this->setSale();
+
+        return $this->sale;
+    }
+}
+
+//Premium users
+class Premium extends User{
+
+    private $extra_sale = 2.5;
+    protected $subscription_year;
+
+    public function __construct($first_name, $last_name, $age, $subscription_year) {
+        parent::__construct($first_name, $last_name, $age);
+
+        $this->subscription_year = $subscription_year;
+    }
+
+    //Set extra sale in base since he is a subscriber and age
+    private function setExtraSale() {
+        $current_year = date('Y');
+        if ($this->age < 70) {
+
+            $this->sale = 5 + ($this->extra_sale * ($current_year - $this->subscription_year ) );
+        }
+        else {
+            $this->sale = $this->sale + ($this->extra_sale * ($current_year - $this->subscription_year));
+        }
+    }
+
+    public function getSale() { 
+        $this->setExtraSale();
+
+        return $this->sale;
+    }
+}
+
+//Employees users
+class Employees extends User {
+
+    private $extra_sale = 10;
+    protected $level;
+
+    public function __construct($first_name, $last_name, $age, $level)
+    {
+        parent::__construct($first_name, $last_name, $age);
+
+        $this->level = $level;
+    }
+
+    //Set extra sale in base of the level
+    private function setExtraSale() {
+        $this->sale = $this->extra_sale * ($this->level);
+    }
+
+    public function getSale() {
+        $this->setExtraSale();
+
+        return $this->sale;
+    }
 }
