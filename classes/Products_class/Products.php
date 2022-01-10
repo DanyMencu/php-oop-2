8e37fb702cfed7b72fdb634dd40201c4794d628e
DanyMencu <dany69men@gmail.com>
2022-01-10 16:15:59 +0100

    Create and test product classes and the inherited classes

--- classes/Products_class/Products.php
+++ classes/Products_class/Products.php
@@ -7,15 +7,90 @@ class Product
 {
     //Property
     protected $name;
-    protected $price;
     protected $type;
-    protected $sale;
+    protected $price;
+    protected $quantity;
+    protected $sale = 0;
 
     //Constructor
-    public function __construct($name, $price, $type)
+    public function __construct($name, $type, $price, $quantity)
     {
         $this->name = $name;
-        $this->price = $price;
         $this->type = $type;
+        $this->price = $price;
+        $this->quantity = $quantity;
+    }
+
+    public function getProduct() {
+        return $this->name . '<br>' . $this->type;
+    }
+
+    //Set the discount based on how many products there are in stock
+    protected function setSale() {
+        if ($this->quantity < 10) {
+            $this->sale = 10;
+        }
+
+        return $this->sale;
+    }
+
+    public function getPrice() {
+        $this->setSale();
+
+        if ($this->sale > 0) {
+            $this->price = $this->price - ( ($this->price * $this->sale) / 100);
+        }
+
+        return number_format($this->price, 2);
+    }
+}
+
+//Clothes
+class Clothes extends Product {
+
+    protected $size;
+    protected $color;
+
+    public function __construct($name, $type, $price, $quantity, $size, $color) {
+        parent::__construct($name, $type, $price, $quantity);
+
+        $this->size = $size;
+        $this->color = $color;
+    }
+
+    public function getDescription() {
+        return $this->size . ' - ' . $this->color;
+    }
+}
+
+//Food
+class Food extends Product {
+
+    protected $month_duration;
+    protected $fresh;
+
+    public function __construct($name, $type, $price, $quantity, $month_duration)
+    {
+        parent::__construct($name, $type, $price, $quantity);
+
+        $this->month_duration = $month_duration;
+    }
+
+    //Set the freshness if the duration is more than 2 month
+    protected function setFreshness() {
+        $this->fresh = $this->month_duration >= 2 ? 'SURGELATO' : 'FRESCO';
+    }
+
+    public function getFreshness() {
+        $this->setFreshness();
+
+        return $this->fresh;
+    }
+
+    //Set price if the product has less than 1 month of duration
+    protected function setSale() {
+        if ($this->month_duration <= 1) {
+            $this->sale = 15;
+        }
     }
 }
--- index.php
+++ index.php
@@ -18,43 +18,95 @@ require_once __DIR__ . '/classes/Users_class/Users.php';
 
 <body>
     <main>
-        <h1>Users type:</h1>
+        <!-- Users type section -->
+        <section>
+            <h1>User types:</h1>
 
-        <!-- Normal -->
-        <div>
-            <h2>Normal</h2>
-            <?php
-            $normal_user = new User('Daniele', 'Menculini', 25);
-            ?>
-            <h4><?php echo $normal_user->getFullName() ?></h4>
-            <p>Lo sconto riservato a te è del:
-                <?php echo $normal_user->getSale() ?>%
-            </p>
-        </div>
+            <!-- Normal -->
+            <div>
+                <h2>Normal</h2>
+                <?php
+                $normal_user = new User('Daniele', 'Menculini', 25);
+                ?>
+                <h4><?php echo $normal_user->getFullName() ?></h4>
+                <p>Lo sconto riservato a te è del:
+                    <?php echo $normal_user->getSale() ?>%
+                </p>
+            </div>
 
-        <!-- Premium -->
-        <div>
-            <h2>Premium</h2>
-            <?php
-            $normal_user = new Premium ('Daniele', 'Menculini', 25, 2014);
-            ?>
-            <h4><?php echo $normal_user->getFullName() ?></h4>
-            <p>Lo sconto riservato a te UTENTE PREMIUM è del:
-                <?php echo $normal_user->getSale() ?>%
-            </p>
-        </div>
+            <!-- Premium -->
+            <div>
+                <h2>Premium</h2>
+                <?php
+                $premium_user = new Premium('Daniele', 'Menculini', 25, 2014);
+                ?>
+                <h4><?php echo $premium_user->getFullName() ?></h4>
+                <p>Lo sconto riservato a te UTENTE PREMIUM è del:
+                    <?php echo $premium_user->getSale() ?>%
+                </p>
+            </div>
 
-        <!-- Employee -->
-        <div>
-            <h2>Employee</h2>
-            <?php
-            $normal_user = new Employee ('Daniele', 'Menculini', 25, 3);
-            ?>
-            <h4><?php echo $normal_user->getFullName() ?></h4>
-            <p>Lo sconto riservato a te DIPENDENTE è del:
-                <?php echo $normal_user->getSale() ?>%
-            </p>
-        </div>
+            <!-- Employee -->
+            <div>
+                <h2>Employee</h2>
+                <?php
+                $employee_user = new Employee('Daniele', 'Menculini', 25, 3);
+                ?>
+                <h4><?php echo $employee_user->getFullName() ?></h4>
+                <p>Lo sconto riservato a te DIPENDENTE è del:
+                    <?php echo $employee_user->getSale() ?>%
+                </p>
+            </div>
+        </section>
+
+        <hr>
+
+        <!-- Product type section-->
+        <section>
+            <h1>Product types:</h1>
+
+            <!-- Normal product -->
+            <div>
+                <h2>Normal</h2>
+                <?php
+                $normal_product = new Product('Polsini da palestra', 'Accessorio', 12, 9)
+                ?>
+                <h4><?php echo $normal_product->getProduct() ?></h4>
+                <p>Il prezzo dell'articolo è:
+                    <?php echo $normal_product->getPrice() ?> €
+                </p>
+            </div>
+
+            <!-- Clothes product -->
+            <div>
+                <h2>Abbigliamento</h2>
+                <?php
+                $clothes_product = new Clothes('Cabottiera Prozis', 'T-Shirt', 19, 4, 'Red', 'L')
+                ?>
+                <h4><?php echo $clothes_product->getProduct() ?></h4>
+                <p>
+                    <?php echo $clothes_product->getDescription() ?>
+                </p>
+                <p>Il prezzo del capo d'abbigliament è:
+                    <?php echo $clothes_product->getPrice() ?> €
+                </p>
+            </div>
+
+            <!-- Clothes product -->
+            <div>
+                <h2>Alimento</h2>
+                <?php
+                $food_product = new Food('Pepperoni pizza', 'Alimento pronto', 25, 9, 2);
+                ?>
+                <h4><?php echo $food_product->getProduct() ?></h4>
+                <p>Il prodotto è: 
+                    <?php echo $food_product->getFreshness() ?>
+                </p>
+                <p>Il prezzo della pietanza è:
+                    <?php echo $food_product->getPrice() ?> €
+                </p>
+            </div>
+        </section>
     </main>
 </body>
 
